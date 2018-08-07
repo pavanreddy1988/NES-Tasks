d20579f825acd1b6e94f9d11365cd827d3f1bb53
pavanreddy1988 <42129677+pavanreddy1988@users.noreply.github.com>
2018-08-07 16:54:05 +0530

    Update README.js

--- README.js
+++ README.js
@@ -1,5 +1,4 @@
 //Run this Code in the EDS CustomLink Mapping Profile Check Page @ http://profile-check.ebsco-gss.net/
-
 var getMain1 = window.document.getElementById('customlinks');
 var getMain2 = Array.prototype.slice.call(getMain1.getElementsByClassName('label label-danger'));
 var cl = [];
